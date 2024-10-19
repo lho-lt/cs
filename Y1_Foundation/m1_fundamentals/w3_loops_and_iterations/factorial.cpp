38e309a5ce7e63d9fa413a6a7bac4be4586c8474
lho <164295110+lho-lt@users.noreply.github.com>
2024-10-19 12:08:56 +0530

    week 3 completed

--- Y1_Foundation/m1_fundamentals/w3_loops_and_iterations/factorial.cpp
+++ Y1_Foundation/m1_fundamentals/w3_loops_and_iterations/factorial.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    int num;                    // Variable to store the input number
-    long long factorial = 1;    // Variable to store the factorial result
+    int num;                 // Variable to store the input number
+    long long factorial = 1; // Variable to store the factorial result
 
     // Prompt the user to enter a number
     std::cout << "Enter your number: ";
@@ -10,13 +10,15 @@ int main() {
     // Check if the input is a valid integer and non-negative
     if (!(std::cin >> num) || num < 0) {
         // If the input is invalid or non-negative, print an error message
-        std::cerr << "Invalid input! Please enter a non-negative integer." << std::endl;
-        return 1;   // Exit with non-zero status to indicate an error
+        std::cerr << "Invalid input! Please enter a non-negative integer."
+                  << std::endl;
+        return 1; // Exit with non-zero status to indicate an error
     }
 
     // If the input is 0 or 1, return the result with 1;
     if (num == 0 || num == 1) {
-        std::cout << "The factorial of " << num << " is " << factorial << std::endl;
+        std::cout << "The factorial of " << num << " is " << factorial
+                  << std::endl;
         return 0;
     }
 
